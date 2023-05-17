7e359a0c80f2a57d940cb33a4946e04cc83f5ac2
rtrehan <ramandeeptrehan@gmail.com>
2023-05-16 23:12:15 -0700

    Add links to different pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 import NavigationBar from "./components/navigationBar/NavigationBar"
 import Footer from "./components/footer/Footer"
+import Home from "./pages/home/Home";
+import Gigs from "./pages/gigs/Gigs";
+import Gig from "./pages/gig/Gig";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import AddGig from "./pages/addGig/AddGig";
+import Orders from "./pages/orders/Orders";
+import Messages from "./pages/messages/Messages";
+import Message from "./pages/message/Message";
+import MyGigs from "./pages/myGigs/MyGigs";
 
 import {
   createBrowserRouter,
@@ -22,11 +32,46 @@ function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Layout/>
+      element: <Layout/>,
 
       //For each page, we will have a different element
 
-    }
+      children: [
+        {
+          path: "/",
+          element: <Home/>
+        },
+        {
+          path: "/gigs",
+          element: <Gigs/>
+        },
+        {
+          path: "/gig/:id",
+          element: <Gig/>
+        },
+        {
+          path: "/orders",
+          element: <Orders/>
+        },
+        {
+          path: "/mygigs",
+          element: <MyGigs/>
+        },
+        {
+          path: "/addGig",
+          element: <AddGig/>
+        },
+        {
+          path: "/messages",
+          element: <Messages/>
+        },
+        {
+          path: "/message/:id",
+          element: <Message/>
+        }
+      ]
+    },
+    
   ]);
 
   return (
@@ -34,33 +79,6 @@ function App() {
       <RouterProvider router={router} />
     </div>
   );
-
-  {/*
-  return (
-    <div>
-      Hello world! 
-      <NavigationBar/>
-      <h1>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-      </h1>
-      <h1>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-      </h1>
-      <h1>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-      </h1>
-      <h1>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-      </h1>
-      <h1>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-      </h1>
-      <h1>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-      </h1>
-    </div>
-  )
-  */}
 }
 
 export default App
---
+++ src/pages/addGig/AddGig.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./AddGig.scss"
+
+const AddGig = () => {
+  return (
+    <div className='addGig'>Add Gig</div>
+  )
+}
+
+export default AddGig
