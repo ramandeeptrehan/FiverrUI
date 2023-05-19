c6adc4ab571d347f7e09f67a658ac3b8c23b7acb
rtrehan <ramandeeptrehan@gmail.com>
2023-05-18 22:23:15 -0700

    Add basic layout of div for featured component

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -5,7 +5,20 @@ const Featured = () => {
   return (
     <div className="featured">
         <div className="container">
-            feature tab is here
+            {/* We have 2 parts of this container: left(text) and right (image) */}
+            <div className="left">
+                {/* Inside left section, we have a title, etc */}
+                <h1>
+                    Find the perfect freelance services for your business
+                </h1>
+                <div className="search">
+                    <div className="searchInput">
+                        <img src="" alt=""></img>
+                        <input type="text" placeholder='Try "building mobile app"'></input>
+                    </div>
+                </div>
+            </div>
+            <div className="right"></div>
         </div>
     </div>
   )
