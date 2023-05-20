3b6dfe26ee26f5edd74ff5b72b974aebb132fb46
rtrehan <ramandeeptrehan@gmail.com>
2023-05-20 14:44:28 -0700

    Use npm package to build Slider in the app

---
+++ src/components/categoryCard/CategoryCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./CategoryCard.scss"
+
+const CategoryCard = () => {
+  return (
+    <div>CategoryCard</div>
+  )
+}
+
+export default CategoryCard
---
+++ src/components/slide/Slide.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Slider from 'infinite-react-carousel'
+import "./Slide.scss"
+
+const Slide = () => {
+  return (
+    <div className="slider">
+        <div className="container">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Slide
